2156284b2f04bcdbe05b743d1ecc986e96eeef66
GokceKahraman <gokcekahraman0@gmail.com>
2022-12-05 02:15:06 +0300

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Gökçe Kahraman on 5.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
